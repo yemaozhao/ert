ed6623c82626f74df3147af8c3b9df0b1824a3e5
yemaozhao <305694161@qq.com>
2017-09-13 20:38:19 +0800

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+import logging
+from django.shortcuts import render
+from django.conf import settings
+
+logger = logging.getLogger('blog.views')
+
+# Create your views here.
+
+def hello(request):
+    return render(request,'hello.html')
+
+def index(request):
+    try:
+        file = open('ss.txt','r')
+    except Exception as e:
+        logger.error(e)
+    return render(request, 'index.html', locals())
+
+def global_setting(request):
+    return {'SITE_NAME': settings.SITE_NAME,
+            'SITE_DESC':settings.SITE_DESC,}
